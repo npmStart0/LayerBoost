ff96da96ebec0820e0f63d78f9e65e0a8e2a6d3c
DvoraShechter1 <d0583212560@gmail.com>
2024-11-02 22:48:16 +0200

    create basic project
    Signed-off-by: DvoraShechter1 <d0583212560@gmail.com>

---
+++ CreateCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lboost
+{
+    public class CreateCommand : Command
+    {
+        public CreateCommand() : base("create", "create an empty 3-layers project")
+        {
+            this.SetHandler(() =>
+            {
+                //here we need write what the command do
+            });
+        }
+        
+
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using lboost;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+var rootCommand = new RootCommand("")
+{
+};
+
+// Add a subcommand
+var createCommand = new CreateCommand
+{
+
+};
+
+rootCommand.AddCommand(createCommand);
+
+await rootCommand.InvokeAsync(args);
